c3ff365649c585d640076a364fa54035b3af6821
jmr <jacky_boy@live.ca>
2017-02-22 11:54:00 -0500

    merge

---
+++ application/logs/log-2017-02-22.php
@@ -0,0 +1,18 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2017-02-22 11:38:06 --> UTF-8 Support Enabled
+DEBUG - 2017-02-22 11:38:06 --> No URI present. Default controller set.
+DEBUG - 2017-02-22 11:38:06 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-02-22 11:38:06 --> Total execution time: 0.0768
+DEBUG - 2017-02-22 11:49:05 --> UTF-8 Support Enabled
+DEBUG - 2017-02-22 11:49:05 --> No URI present. Default controller set.
+DEBUG - 2017-02-22 11:49:05 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-02-22 11:49:05 --> Total execution time: 0.0448
+DEBUG - 2017-02-22 11:49:06 --> UTF-8 Support Enabled
+DEBUG - 2017-02-22 11:49:06 --> No URI present. Default controller set.
+DEBUG - 2017-02-22 11:49:07 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-02-22 11:49:07 --> Total execution time: 0.0432
+DEBUG - 2017-02-22 11:49:08 --> UTF-8 Support Enabled
+DEBUG - 2017-02-22 11:49:08 --> No URI present. Default controller set.
+DEBUG - 2017-02-22 11:49:08 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-02-22 11:49:09 --> Total execution time: 0.0420
